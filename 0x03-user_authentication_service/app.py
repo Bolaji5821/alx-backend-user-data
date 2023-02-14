385332b6608967343c1cbf4eb7b353c7d1afd97f
Bolaji581 <oyedelebolaji84@gmail.com>
2023-02-14 08:05:35 +0100

    routed add user method in Flask App

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,5 +32,6 @@ def user() -> str:
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
