f942bc11ecef74da7c1fb569d157320654ee8c29
Bolaji581 <oyedelebolaji84@gmail.com>
2023-02-14 08:38:53 +0100

    updated login session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,6 +41,7 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
     def valid_login(self, email: str, password: str) -> bool:
         """
         valid_login.
@@ -50,7 +51,6 @@ class Auth:
         except NoResultFound:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
-
 
     def create_session(self, email: str) -> str:
         """
